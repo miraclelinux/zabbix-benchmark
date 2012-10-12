e29c286ddec4f4257631f6e38d7e8fec27b39a68
Takuro Ashie <ashie@clear-code.com>
2012-10-12 11:02:52 +0900

    add valid params for host.create of API 1.3 & 1.4

--- benchmark/zabbix-benchmark.rb
+++ benchmark/zabbix-benchmark.rb
@@ -2,6 +2,23 @@
 require 'rubygems'
 require 'zbxapi'
 
+class Host < ZabbixAPI_Base
+  action :create do
+    add_valid_params("1.3",
+                     ["host","port","status","useip",
+                      "dns","ip","proxy_hostid",
+                      "useipmi","ipmi_ip","ipmi_port", "ipmi_authtype",
+                      "ipmi_privilege","ipmi_username", "ipmi_password",
+                      "groups","templates"])
+    add_valid_params("1.4",
+                     ["host","status",
+                      "proxy_hostid","useipmi","ipmi_ip","ipmi_port",
+                      "ipmi_authtype","ipmi_privilege","ipmi_username",
+                      "ipmi_password","groups","templates","interfaces"])
+  end
+end
+
+
 class Benchmark < ZabbixAPI
   def initialize
     @server_url = "http://localhost:8080/zabbix/"
