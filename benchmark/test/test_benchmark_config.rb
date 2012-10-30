b054c0b3de6eb02293272315b0500d3289f317f0
Takuro Ashie <ashie@clear-code.com>
2012-10-30 16:50:59 +0900

    add some tests for config file.

--- benchmark/test/test_benchmark_config.rb
+++ benchmark/test/test_benchmark_config.rb
@@ -5,9 +5,26 @@ require '../lib/benchmark-config.rb'
 class BenchmarkConfigTestCase < Test::Unit::TestCase
   def setup
     @config = BenchmarkConfig.instance.reset
+    @fixture_file = "fixtures/config.yml"
   end
 
   def teardown
+  end
+
+  def test_uri_from_file
+    @config.load_file(@fixture_file)
+    assert_equal("http://localhost:8080/zabbix-postgresql/", @config.uri)
+  end
+
+  def test_warmup_duration_from_file
+    @config.load_file(@fixture_file)
+    assert_equal(777, @config.warmup_duration)
+  end
+
+  def test_agents_from_file
+    expected = [{"ip_address" => "192.168.1.10", "port" => 10052}]
+    @config.load_file(@fixture_file)
+    assert_equal(expected, @config.agents)
   end
 
   def test_default_hosts_step
