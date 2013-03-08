06a51972163d24c06161476ee61eb4f4fe635eb0
Takuro Ashie <takuro.ashie@miraclelinux.com>
2013-03-08 14:39:25 +0900

    @rows[i + 1] -> next_row

--- lib/benchmark-result.rb
+++ lib/benchmark-result.rb
@@ -167,8 +167,9 @@ class ReadLatencyLog < BenchmarkResult
 
     @rows.each_with_index do |row, i|
       rows_one_step.push(row)
+      next_row = @rows[i + 1]
       n_items = row[N_ITEMS_COLUMN].to_i
-      n_items_next = @rows[i + 1] ? @rows[i + 1][N_ITEMS_COLUMN].to_i : -1
+      n_items_next = next_row ? next_row[N_ITEMS_COLUMN].to_i : -1
       if n_items_next != n_items
         statistics.push(analyze_statistics_one_step(rows_one_step))
         rows_one_step = []
