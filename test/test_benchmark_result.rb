0ce7963414b765c46aed775622cf08eef3177905
Takuro Ashie <takuro.ashie@miraclelinux.com>
2013-03-08 18:07:14 +0900

    Add skeleton of a test case for BenchmarkResult.

---
+++ test/test_benchmark_result.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+gem 'test-unit'
+require 'test/unit'
+require '../lib/benchmark-result.rb'
+
+class BenchmarkResultTestCase < Test::Unit::TestCase
+  def setup
+  end
+
+  def teardown
+  end
+end
+
