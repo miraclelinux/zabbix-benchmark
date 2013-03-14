0c1ae492c845bbed8a0b91b5d1c819de9bfc4826
Takuro Ashie <takuro.ashie@miraclelinux.com>
2013-03-14 14:45:14 +0900

    Mock ZabbixLog.

--- test/test_zabbix_benchmark.rb
+++ test/test_zabbix_benchmark.rb
@@ -7,7 +7,7 @@ require 'zabbix-benchmark-test-util'
 require 'zabbix-benchmark'
 
 class ZabbixBenchmarkChild <  ZabbixBenchmark
-  attr_accessor :zabbix
+  attr_accessor :zabbix, :zabbix_log
 end
 
 class ZabbixBenchmarkTestCase < Test::Unit::TestCase
@@ -15,8 +15,11 @@ class ZabbixBenchmarkTestCase < Test::Unit::TestCase
 
   def setup
     @zabbix = ZbxAPIUtils.new("http://test-host/", "Admin", "zabbix")
+    @zabbix_log = ZabbixLog.new(nil)
     stub(@zabbix).ensure_loggedin
     stub(@zabbix).API_version { "1.4\n" }
+    stub(@zabbix_log).parse
+    stub(@zabbix_log).rotate
     @benchmark = benchmark
   end
 
@@ -27,6 +30,7 @@ class ZabbixBenchmarkTestCase < Test::Unit::TestCase
   def benchmark
     benchmark = ZabbixBenchmarkChild.new
     benchmark.zabbix = @zabbix
+    benchmark.zabbix_log = @zabbix_log
     benchmark
   end
 
@@ -94,7 +98,7 @@ class ZabbixBenchmarkTestCase < Test::Unit::TestCase
     end
   end
 
-  def expected_output_one_step(n_hosts, base, warmup, measure)
+  def expected_output_one_step(n_hosts, base, warmup, measure, results)
     names = hostnames(n_hosts, base)
     <<EOS
 Enable #{names.length} dummy hosts: 
@@ -106,15 +110,17 @@ Enabled items: #{(names.length + base) * 2}
 Warmup #{warmup} seconds ...
 Measuring write performance for #{measure} seconds ...
 Collecting results ...
-DBsync average: NaN [msec/item]
-Total 0 items are written
+DBsync average: #{results[0]} [msec/item]
+Total #{results[1]} items are written
 EOS
   end
 
-  def expected_output(n_steps, step, warmup, measure)
+  def expected_output(n_steps, step, warmup, measure, results)
     output = ""
     n_steps.times do |i|
-      output += expected_output_one_step(step, step * i, warmup, measure)
+      output += expected_output_one_step(step, step * i,
+                                         warmup, measure,
+                                         results[i])
       output += "\n"
     end
     output += "Disable all dummy hosts ...\n"
@@ -127,15 +133,22 @@ EOS
     config.warmup_duration = 0.3
     config.measurement_duration = 0.6
 
+    dummy_results = [
+      [0.123, 234, 28.782],
+      [0.321, 432, 138.672]
+    ]
+
     mock(@zabbix).get_enabled_test_hosts{[]}.once
 
     mock(@zabbix).enable_hosts(hostnames(3)).once
     mock(@zabbix).get_enabled_hosts { hosts(3) }.once
     mock(@zabbix).get_enabled_items(hostids(3)) { items(6) }.once
+    mock(@zabbix_log).dbsyncer_total{dummy_results[0]}.once
 
     mock(@zabbix).enable_hosts(hostnames(3, 3)).once
     mock(@zabbix).get_enabled_hosts { hosts(6) }.once
     mock(@zabbix).get_enabled_items(hostids(6)) { items(12) }.once
+    mock(@zabbix_log).dbsyncer_total{dummy_results[1]}.once
 
     mock(@zabbix).disable_hosts(hostnames(6)).once
 
@@ -143,6 +156,6 @@ EOS
       benchmark.run_without_setup
     end
 
-    assert_equal(expected_output(2, 3, 0.3, 0.6), output)
+    assert_equal(expected_output(2, 3, 0.3, 0.6, dummy_results), output)
   end
 end
