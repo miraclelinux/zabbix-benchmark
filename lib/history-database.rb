213789314d6429ca05213404da77d71f5f2a7817
Takuro Ashie <takuro.ashie@miraclelinux.com>
2013-04-16 14:29:52 +0900

    Move insert_query_for_one_day method from HistoryMySQL to parent.
    It can also be used for other DBs which supports multiple insert.

--- lib/history-database.rb
+++ lib/history-database.rb
@@ -47,6 +47,19 @@ class HistoryDatabase
       ['history', '1.0', conf["interval_string"]]
     end
   end
+
+  def insert_query_for_one_day(item, clock_offset)
+    itemid = item["itemid"].to_i
+    table, value, interval = params_for_value_type(item["value_type"].to_i)
+    last_clock = clock_offset + 60 * 60 * 24 - interval
+    query = "INSERT INTO #{table} (itemid, clock, ns, value) VALUES "
+    clock_offset.step(last_clock, interval) do |clock|
+      query += "(#{itemid}, #{clock}, 0, #{value})"
+      query += ", " if clock < last_clock
+    end
+    query += ";"
+    query
+  end
 end
 
 class HistoryMySQL < HistoryDatabase
@@ -87,20 +100,6 @@ class HistoryMySQL < HistoryDatabase
       query = insert_query_for_one_day(item, clock_offset);
       @mysql.query(query)
     end
-  end
-
-  private
-  def insert_query_for_one_day(item, clock_offset)
-    itemid = item["itemid"].to_i
-    table, value, interval = params_for_value_type(item["value_type"].to_i)
-    last_clock = clock_offset + 60 * 60 * 24 - interval
-    query = "INSERT INTO #{table} (itemid, clock, ns, value) VALUES "
-    clock_offset.step(last_clock, interval) do |clock|
-      query += "(#{itemid}, #{clock}, 0, #{value})"
-      query += ", " if clock < last_clock
-    end
-    query += ";"
-    query
   end
 end
 
